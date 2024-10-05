0ad7ea14a22505fe57eeb98d437c7c50c777c7d8
Thiago <matheusthiago822@gmail.com>
2024-10-05 06:56:06 -0300

    otimizacao

--- script.py
+++ script.py
@@ -2,34 +2,24 @@ import os
 from pathlib import Path
 current=os.getcwd()
 video_format=input('Formato de vídeo: ')
-files=list(Path(current).glob(f'*.{video_format}'))
+files1=list(Path(current).glob(f'*.{video_format}'))
+files2=files=list(Path(current).glob('*srt'))
 i=1
 if (len(files)==0):
-    print('Não existem arquivos')
+    print('Não existem arquivos.')
 else:
-    for file in files:
-        if 'E01' in str(file):
-            a=str(file)
-            a=a.split('\\')
-            ext1='mp4'
-            ext2='srt'
-            files=list(Path(current).glob('*srt'))
-            break
-        else:
-            files=list(Path(current).glob('*srt'))
-    for file in files:
-        if 'E01' in str(file):
-            a=str(file)
-            a=a.split('\\')
-            ext1='srt'
-            ext2='mp4'
-            files=list(Path(current).glob(f'*.{video_format}'))
-            break
+    a=str(files1[0]).split('\\')
+    if 'E01' in str(files1[0]):
+        ext1=video_format
+        ext2='srt'
+    elif 'E01' in str(files2[0]):
+        ext1='srt'
+        ext2=video_format
+    else:
+        print('Não existe padrão sequencial nos títulos.')
 for x in a:
-    if (f'{video_format}') in x:
+    if ((f'{video_format}') in x) or ('srt' in x):
         a=x
-    elif 'srt' in x:
-        a=x        
 for file in files:
     title=a.replace('E01',f'E{str(i).zfill(2)}')
     title=title.replace(ext1,ext2)
