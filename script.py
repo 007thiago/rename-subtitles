ffb7b786af4d4f3661c277ab0096342d14497287
Thiago <matheusthiago822@gmail.com>
2024-10-05 07:49:54 -0300

    -

--- script.py
+++ script.py
@@ -2,22 +2,24 @@ import os
 from pathlib import Path
 current=os.getcwd()
 video_format=input('Formato de vídeo: ')
-files1=list(Path(current).glob(f'*.{video_format}'))
-files2=files=list(Path(current).glob('*srt'))
+files=list(Path(current).glob('*srt'))
 i=1
 if (len(files)==0):
     print('Não existem arquivos.')
 else:
-    a=str(files1[0]).split('\\')
-    a=a[len(a)-1]
-    if 'E01' in str(files1[0]):
-        ext1=video_format
-        ext2='srt'
-    elif 'E01' in str(files2[0]):
+    if 'E01' in str(files[0]):
+        a=str(files[0]).split('\\')
         ext1='srt'
         ext2=video_format
+        files=list(Path(current).glob(f'*{video_format}'))
     else:
-        print('Não existe padrão sequencial nos títulos.')
+        files=list(Path(current).glob(f'*{video_format}'))
+        if 'E01' in str(files[0]):
+            a=str(files[0]).split('\\')
+            ext1=video_format
+            ext2='srt'
+            files=list(Path(current).glob('*srt'))
+a=a[len(a)-1]
 for file in files:
     title=a.replace('E01',f'E{str(i).zfill(2)}')
     title=title.replace(ext1,ext2)
