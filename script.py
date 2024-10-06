97d7418c42275dbe9ba3fa9794c2a614baed6c29
Thiago <matheusthiago822@gmail.com>
2024-10-06 15:54:22 -0300

    funcao para renomear caso nao exista padrao nos titulos

--- script.py
+++ script.py
@@ -2,26 +2,42 @@ import os
 from pathlib import Path
 current=os.getcwd()
 video_format=input('Formato de vídeo: ')
-files=list(Path(current).glob('*srt'))
+files1=list(Path(current).glob('*srt'))
+files2=list(Path(current).glob(f'*{video_format}'))
 i=1
-if (len(files)==0):
+if (len(files1)==0) or (len(files2)==0):
     print('Não existem arquivos.')
 else:
-    if 'E01' in str(files[0]):
-        a=str(files[0]).split('\\')
+    if ('E01' not in str(files1[0])) and ('E01' not in str(files2[0])):
+        a=str(files1[0]).split('\\')
+        a='E01_' + a[len(a)-1]
+        for file in files1:
+            title=a.replace('E01_',f'E{str(i).zfill(2)}_')
+            os.rename(file,title)
+            i+=1
         ext1='srt'
         ext2=video_format
-        files=list(Path(current).glob(f'*{video_format}'))
+        i=1
+        for file in files2:
+            title=a.replace('E01_',f'E{str(i).zfill(2)}_')
+            title=title.replace(ext1,ext2)
+            os.rename(file,title)
+            i+=1
     else:
-        files=list(Path(current).glob(f'*{video_format}'))
-        if 'E01' in str(files[0]):
-            a=str(files[0]).split('\\')
+        if ('E01' in str(files1[0])):
+            ext1='srt'
+            ext2=video_format
+            files=files2
+            a=str(files1[0]).split('\\')
+        else:
             ext1=video_format
             ext2='srt'
-            files=list(Path(current).glob('*srt'))
-a=a[len(a)-1]
-for file in files:
-    title=a.replace('E01',f'E{str(i).zfill(2)}')
-    title=title.replace(ext1,ext2)
-    os.rename(file,title)
-    i+=1
+            files=files1
+            a=str(files2[0]).split('\\')
+        a=a[len(a)-1]
+        i=1
+        for file in files:
+            title=a.replace('E01',f'E{str(i).zfill(2)}')
+            title=title.replace(ext1,ext2)
+            os.rename(file,title)
+            i+=1
