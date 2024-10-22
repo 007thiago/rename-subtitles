af29d70565eb0feaa2c3a3aadd383f4d13cc1111
Thiago <matheusthiago822@gmail.com>
2024-10-22 16:08:46 -0300

    -

--- script.py
+++ script.py
@@ -1,37 +1,37 @@
 import os
 from pathlib import Path
 current=os.getcwd()
-video_format=input('Formato de vídeo: ')
-files1=list(Path(current).glob('*srt'))
-files2=list(Path(current).glob(f'*{video_format}'))
+videoF=input('Formato de vídeo: ')
+subF=input('Formato de legenda: ')
+files1=list(Path(current).glob(f'*{subF}'))
+files2=list(Path(current).glob(f'*{videoF}'))
+files3=files1+files2
+s=len(files1)
 i=1
 if (len(files1)==0) or (len(files2)==0):
-    print('Não existem arquivos.')
+    print('Não existem arquivos de vídeo/legenda.')
 else:
     if ('E01' not in str(files1[0])) and ('E01' not in str(files2[0])):
         a=str(files1[0]).split('\\')
         a='E01_' + a[len(a)-1]
-        for file in files1:
+        for file in files3:
+            if (i==(s+1)):
+                i=1
+                ext1=subF
+                ext2=videoF
+                a=a.replace(ext1,ext2)
             title=a.replace('E01_',f'E{str(i).zfill(2)}_')
-            os.rename(file,title)
-            i+=1
-        ext1='srt'
-        ext2=video_format
-        i=1
-        for file in files2:
-            title=a.replace('E01_',f'E{str(i).zfill(2)}_')
-            title=title.replace(ext1,ext2)
             os.rename(file,title)
             i+=1
     else:
         if ('E01' in str(files1[0])):
-            ext1='srt'
-            ext2=video_format
+            ext1=subF
+            ext2=videoF
             files=files2
             a=str(files1[0]).split('\\')
         else:
-            ext1=video_format
-            ext2='srt'
+            ext1=videoF
+            ext2=subF
             files=files1
             a=str(files2[0]).split('\\')
         a=a[len(a)-1]
