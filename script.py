42d4085f66a1b44c0cccd444bcd7369e86389a3a
Thiago <matheusthiago822@gmail.com>
2024-10-27 10:43:45 -0300

    -

--- script.py
+++ script.py
@@ -1,20 +1,22 @@
 import os
 from pathlib import Path
 current=os.getcwd()
-videoF=input('Formato de vídeo: ')
-subF=input('Formato de legenda: ')
+videoF=input('Video format: ')
+subF=input('Subtitle format: ')
 files1=list(Path(current).glob(f'*{subF}'))
 files2=list(Path(current).glob(f'*{videoF}'))
 files3=files1+files2
 s=len(files1)
 i=1
 if (len(files1)==0) or (len(files2)==0):
-    print('Não existem arquivos de vídeo/legenda.')
+    print('There\'re no video/subtitle files.')
 else:
+    #If there isn't a default rule on videos/subtitles titles, one will be created
     if ('E01' not in str(files1[0])) and ('E01' not in str(files2[0])):
         a=str(files1[0]).split('\\')
         a='E01_' + a[len(a)-1]
         for file in files3:
+            #If all subtitles files are already renamed, then counter resets in order to iterate through video files, and the source extension is now subtitle (subtitle -> video file)
             if (i==(s+1)):
                 i=1
                 ext1=subF
@@ -25,19 +27,17 @@ else:
             i+=1
     else:
         if ('E01' in str(files1[0])):
-            ext1=subF
-            ext2=videoF
-            files=files2
             a=str(files1[0]).split('\\')
+            files=files2
+            ext1,ext2=subF,videoF
         else:
-            ext1=videoF
-            ext2=subF
-            files=files1
             a=str(files2[0]).split('\\')
+            files=files1
+            ext1,ext2=videoF,subF
         a=a[len(a)-1]
         i=1
         for file in files:
-            title=a.replace('E01',f'E{str(i).zfill(2)}')
-            title=title.replace(ext1,ext2)
+            title=a.replace(ext1,ext2)
+            title=title.replace('E01',f'E{str(i).zfill(2)}')
             os.rename(file,title)
             i+=1
